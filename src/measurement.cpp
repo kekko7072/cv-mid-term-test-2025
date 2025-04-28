3cb5713dd24a2fb65e1952e7b175a23332b701d9
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2025-04-29 00:02:46 +0200

    Fixed issue on measurements.

--- src/measurement.cpp
+++ src/measurement.cpp
@@ -15,11 +15,22 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+vector<fs::path> list_files(const fs::path& directory) {
+    vector<fs::path> files;
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (entry.is_regular_file()) {
+            files.push_back(entry.path());
+        }
+    }
+    sort(files.begin(), files.end()); // Important: sort alphabetically
+    return files;
+}
+
 void performance_evaluation(OBJTYPE obj_type, const fs::path &dataset_dir)
 {
     string obj_type_str;
 
-    switch (obj_type){
+    switch (obj_type) {
         case OBJTYPE::BOX:
             obj_type_str = "004_sugar_box";
             break;
@@ -36,27 +47,33 @@ void performance_evaluation(OBJTYPE obj_type, const fs::path &dataset_dir)
     const fs::path prediction_dir = dataset_dir / obj_type_str / "output";
     const fs::path dataset_path = dataset_dir / obj_type_str / "labels";
 
-    int number_of_files = number_images(obj_type_str, dataset_dir);
+    vector<fs::path> label_files = list_files(dataset_path);
+    vector<fs::path> prediction_files = list_files(prediction_dir);
 
+    if (label_files.size() != prediction_files.size()) {
+        cerr << "Mismatch between number of label and prediction files!" << endl;
+        return;
+    }
+
+    int number_of_files = label_files.size();
     vector<float> iou(number_of_files);
 
     int temp_acc = 0;
     float miou, acc;
 
-    for(int i = 0; i < number_of_files; i++){
-        iou[i] = IoU(obj_type_str, dataset_path, prediction_dir);
-        temp_acc = accuracy(iou[i]);
+    for (int i = 0; i < number_of_files; i++) {
+        iou[i] = IoU(obj_type_str, label_files[i], prediction_files[i]);
+        temp_acc += accuracy(iou[i]);
     }
 
     miou = mIoU(iou, number_of_files);
-    acc = temp_acc / number_of_files;
+    acc = (float)temp_acc / number_of_files;  // Cast to float for correct result
 
     write_performance(obj_type_str, dataset_dir, miou, acc);
 
     cout << "mean Intersection over Union: " << miou << endl;
     cout << "Accuracy: " << acc << endl;
 }
-
 int box_area(std::string obj_type_str, const fs::path &text_file)
 {
     string obj_name, line;      // obj_name to compare the name of the object we are evaluating, line to read one string at the time if there are more than one object in the text file
@@ -122,25 +139,16 @@ int intersection_area(string obj_type_str, const fs::path &dataset_box, const fs
         cerr << "Unable to open the file: " << predicted_box << endl;
         return -1;
     }
-    
-    int x_inters_min, y_inters_min, x_inters_max, y_inters_max;
-    int width_inters, height_inters, area_inters;
 
-    x_inters_min = min(x_min_gt, x_min_pred);
-    y_inters_min = min(y_min_gt, y_min_pred);
-    x_inters_max = max(x_max_gt, x_max_pred);
-    y_inters_max = max(y_max_gt, y_max_pred);
+    int x_inters_min = max(x_min_gt, x_min_pred);
+    int y_inters_min = max(y_min_gt, y_min_pred);
+    int x_inters_max = min(x_max_gt, x_max_pred);
+    int y_inters_max = min(y_max_gt, y_max_pred);
 
-    if((x_inters_min > x_inters_max) || (y_inters_min > y_inters_max)){     // Check if there is an intersection
-        area_inters = 0;
-    } else{
-        width_inters = x_inters_max - x_inters_min;
-        height_inters = y_inters_max - y_inters_min;
+    int width_inters = max(0, x_inters_max - x_inters_min);
+    int height_inters = max(0, y_inters_max - y_inters_min);
 
-        area_inters = width_inters * height_inters;
-    }
-
-    return area_inters;
+    return width_inters * height_inters;
 }
 
 int union_area(string obj_type_str, const fs::path &dataset_box, const fs::path &predicted_box)
@@ -158,14 +166,11 @@ int union_area(string obj_type_str, const fs::path &dataset_box, const fs::path 
 
 float IoU(string obj_type_str, const fs::path &dataset_box, const fs::path &predicted_box)
 {
-    int area_inters, area_union;
+    int area_inters = intersection_area(obj_type_str, dataset_box, predicted_box);
+    int area_union = union_area(obj_type_str, dataset_box, predicted_box);
 
-    area_inters = intersection_area(obj_type_str, dataset_box, predicted_box);
-    area_union = union_area(obj_type_str, dataset_box, predicted_box);
-
-    float iou_value = area_inters / area_union;
-
-    return iou_value;
+    if(area_union == 0) return 0.0f; // avoid division by zero
+    return (float)area_inters / (float)area_union;
 }
 
 int number_images(string obj_type_str, const fs::path &dataset_dir)
